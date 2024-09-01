c80a82f5cd1d2f37207f577caeb1262cd52cce6e
Leon <lurixlp.yt.alone.for.best@gmail.com>
2024-09-01 19:12:17 +0200

    Getting started

---
+++ Discord Bot/Program.cs
@@ -0,0 +1,26 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+public class Program
+{
+    private static DiscordSocketClient _client;
+    
+    public static async Task Main()
+    {
+        _client = new DiscordSocketClient();
+        _client.Log += Log;
+        
+        const string token = "MTI3OTgzODY3Mjk1MDM5OTA2OQ.GrVZnO.dr0PIqqe5qxCRlzG-xGQqVKAHyQ_B0kkfe3tIQ";
+        
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+        
+        await Task.Delay(-1);
+    }
+    
+    private static Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
